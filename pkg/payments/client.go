df1ec9d40b9dd4eb5fcec02f56cccb699b53d0c7
Geoffrey Ragot <geoffrey.ragot@gmail.com>
2022-09-12 17:04:47 +0200

    fix: Install connector command.

--- pkg/payments/client.go
+++ pkg/payments/client.go
@@ -9,8 +9,8 @@ import (
 
 func NewClient(profile *fctl.Profile, debug bool, organization, stack string) *client.APIClient {
 	configuration := client.NewConfiguration()
-	configuration.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", profile.Token))
-	configuration.Servers[0].URL = fctl.MustApiUrl(profile, organization, stack, "ledger").String()
+	configuration.AddDefaultHeader("Authorization", fmt.Sprintf("Bearer %s", profile.Token.AccessToken))
+	configuration.Servers[0].URL = fctl.MustApiUrl(profile, organization, stack, "payments").String()
 	configuration.Debug = debug
 	return client.NewAPIClient(configuration)
 }
