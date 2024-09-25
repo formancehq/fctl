189d8f8faef2b55bf70f19e6bf6a460204221277
Maxence Maireaux <maxence@maireaux.fr>
2024-09-25 15:09:44 +0200

    refactor: Switch from monorepo

--- .github/actions/extract-in-tag/version.js
+++
@@ -1,8 +0,0 @@
-const value = process.argv[2];
-
-const getValue = (string) => {
-    const value = string.match(/([a-z].*)\/([a-z].*)\/([a-z].*)/)[3];
-    return value;
-};
-
-console.log(getValue(value));
