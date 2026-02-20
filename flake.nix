0d15c7fce010ce823431301551a9e6ca9dde9913
Sylvain Rabot <sylvain@formance.com>
2026-02-20 18:42:35 +0100

    chore: upgrade speakeasy to v1.723.0 (#120)
    Signed-off-by: Sylvain Rabot <sylvain@formance.com>

--- flake.nix
+++ flake.nix
@@ -37,7 +37,7 @@
           f { pkgs = pkgs; pkgs-unstable = pkgs-unstable; system = system; }
         );
 
-      speakeasyVersion = "1.690.0";
+      speakeasyVersion = "1.723.0";
       speakeasyPlatforms = {
         "x86_64-linux"   = "linux_amd64";
         "aarch64-linux"  = "linux_arm64";
@@ -45,10 +45,10 @@
         "aarch64-darwin" = "darwin_arm64";
       };
       speakeasyHashes = {
-        "x86_64-linux"   = "d0943a498f8df18fe85097f514d3b900beb56738f93e760235f8180def1bbd0a";
-        "aarch64-linux"  = "cbdcfc032735c17b71714622743510737811ece0a4cd540ef4a6e6b46ec65075";
-        "x86_64-darwin"  = "ee8806f46fdc6655a957f5220f7904b6de69afd1f96f7865de1bfecd92054541";
-        "aarch64-darwin" = "33730320d768c89bf631fdc2bc8fd3d2c425c3a1015ae4224495ef91e3f883d7";
+        "x86_64-linux"   = "ad92ea76747edc166a08f06be50c7c46de85edad574cb3d43c00d1a8a69d0680";
+        "aarch64-linux"  = "e81ea64194ea6986cbfaac068a16882705843f0db947273f3e62ad0b93cc2852";
+        "x86_64-darwin"  = "92ce41a65713426cea1897af554cf0693cbc0b273b187bc730fb1892f6edac73";
+        "aarch64-darwin" = "f1c5832f982af1f4932c2289d9111afa8b92c138736a0cbb0c9b12abc443ec7b";
       };
 
     in
