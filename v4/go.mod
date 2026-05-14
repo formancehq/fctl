b030fca72e740af8b01097586f357fd33454173f
Maxence Maireaux <maxence@maireaux.fr>
2026-05-14 10:28:34 +0200

    feat: add v4 capabilities model

--- v4/go.mod
+++ v4/go.mod
@@ -6,6 +6,7 @@ toolchain go1.25.7
 
 require (
 	github.com/spf13/cobra v1.10.2
+	golang.org/x/mod v0.36.0
 	gopkg.in/yaml.v3 v3.0.1
 )
 
--- v4/internal/capabilities/doc.go
+++
@@ -1,3 +0,0 @@
-// Package capabilities models stack component versions, API namespaces, and
-// generated operation metadata used by the v4 runtime resolver.
-package capabilities
