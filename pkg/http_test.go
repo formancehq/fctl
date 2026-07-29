69d8a5afd53e8395ea3482308ccec00945b142ed
Maxence Maireaux <maxence@formance.com>
2026-07-29 21:47:37 +0200

    test(http): avoid proxy environment cache

--- pkg/http_test.go
+++ pkg/http_test.go
@@ -2,17 +2,13 @@ package fctl
 
 import (
 	"net/http"
+	"reflect"
 	"testing"
 
 	"github.com/spf13/cobra"
 )
 
 func TestNewHTTPTransportUsesProxyFromEnvironment(t *testing.T) {
-	t.Setenv("HTTP_PROXY", "http://proxy.example:8080")
-	t.Setenv("HTTPS_PROXY", "http://secure-proxy.example:8443")
-	t.Setenv("NO_PROXY", "internal.example")
-	t.Setenv("REQUEST_METHOD", "")
-
 	cmd := &cobra.Command{}
 
 	roundTripper := NewHTTPTransport(cmd)
@@ -28,47 +24,7 @@ func TestNewHTTPTransportUsesProxyFromEnvironment(t *testing.T) {
 	if transport.Proxy == nil {
 		t.Fatal("expected HTTP transport to use proxy settings from the environment")
 	}
-
-	testCases := []struct {
-		name       string
-		requestURL string
-		wantProxy  string
-	}{
-		{
-			name:       "HTTP proxy",
-			requestURL: "http://api.example",
-			wantProxy:  "http://proxy.example:8080",
-		},
-		{
-			name:       "HTTPS proxy",
-			requestURL: "https://api.example",
-			wantProxy:  "http://secure-proxy.example:8443",
-		},
-		{
-			name:       "no proxy",
-			requestURL: "https://internal.example",
-		},
-	}
-
-	for _, testCase := range testCases {
-		t.Run(testCase.name, func(t *testing.T) {
-			request, err := http.NewRequest(http.MethodGet, testCase.requestURL, nil)
-			if err != nil {
-				t.Fatalf("create request: %v", err)
-			}
-
-			proxyURL, err := transport.Proxy(request)
-			if err != nil {
-				t.Fatalf("resolve proxy: %v", err)
-			}
-
-			gotProxy := ""
-			if proxyURL != nil {
-				gotProxy = proxyURL.String()
-			}
-			if gotProxy != testCase.wantProxy {
-				t.Fatalf("expected proxy %q, got %q", testCase.wantProxy, gotProxy)
-			}
-		})
+	if reflect.ValueOf(transport.Proxy).Pointer() != reflect.ValueOf(http.ProxyFromEnvironment).Pointer() {
+		t.Fatal("expected HTTP transport proxy to be http.ProxyFromEnvironment")
 	}
 }
