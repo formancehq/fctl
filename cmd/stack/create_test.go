f805fbcf94d45b29ed185f4bfc0cedc87edc480b
Maxence Maireaux <maxence@formance.com>
2026-08-05 10:17:52 +0200

    test(stack): cover deterministic version ordering

--- cmd/stack/create_test.go
+++ cmd/stack/create_test.go
@@ -12,6 +12,9 @@ func TestSortVersionsDescending(t *testing.T) {
 		"v3.0.0-rc.1",
 		"v2.10.0",
 		"v3.0.0",
+		"v1.0.0+build.1",
+		"v1.0.0+build.2",
+		"preview",
 		"development",
 	}
 
@@ -22,6 +25,9 @@ func TestSortVersionsDescending(t *testing.T) {
 		"v3.0.0-rc.1",
 		"v2.10.0",
 		"v2.9.0",
+		"v1.0.0+build.2",
+		"v1.0.0+build.1",
+		"preview",
 		"development",
 	}, versions)
 }
